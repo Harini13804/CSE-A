4d02f158c2b1d8e82b997a9fad54b4364dd2abc8
Harini13804 <130372108+Harini13804@users.noreply.github.com>
2023-06-21 04:26:44 +0000

    t1

---
+++ cseA/t1.py
@@ -0,0 +1,5 @@
+em=input("Enter your email id:")
+un=em.split("@")[0]
+print("The user name is",un)
+dn=em.split("@")[1]
+print("The domain name is",dn)
---
+++ cseA/t2.py
@@ -0,0 +1,3 @@
+l=(1,2,-23,-33,-2,23,22)
+pn = [num for num in l if num >= 0]
+print(pn)
